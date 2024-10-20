aad206cd77ed4f37837e233d885d5890fc1ff3c8
Kalea Cesaire <121074862+kcesaire@users.noreply.github.com>
2024-10-19 19:35:23 -0700

    Add files via upload
    Journal 1, Question 1

---
+++ Journal1-1.py
@@ -0,0 +1,14 @@
+def main():
+    #print full name
+    print("Kalea Cesaire")
+    
+    #print prfered pronouns
+    print("She/her")
+    
+    #print two sentences about favorite movie and favorite food
+    print("My favorite movie is Nope, its a thriller/horror movie about aliens with social commentary on the treatment of animals in the entertainment industry. Before watching this movie I loved monkies, but now I'm scared of them!")
+    print("My favorite food is california burrito. It has carne asada, fries, cheese, and some other stuff. Eating california burritos always makes me happy!")
+    
+#run main
+if __name__=="__main__":
+    main()
