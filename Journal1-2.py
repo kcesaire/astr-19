f167512afd25b1d09c9ae63fbda2fefce9dc484a
Kalea Cesaire <121074862+kcesaire@users.noreply.github.com>
2024-10-19 20:19:15 -0700

    Add files via upload
    Add Journal 1, Questions 2 & 3

---
+++ Journal1-2.py
@@ -0,0 +1,19 @@
+def main():
+    #Print sum of two floating point numbers
+    float_sum = 2.5 + 7
+    print(float_sum)
+    print(type(float_sum))
+    
+    #Print difference between two integers
+    int_diff = 6-9
+    print(int_diff)
+    print(type(int_diff))
+    
+    #Print product of a floating point number and an integer
+    product = 3.7*6
+    print(product)
+    print(type(product))
+    
+#run program
+if __name__=="__main__":
+    main()
---
+++ Journal1-3.py
@@ -0,0 +1,11 @@
+def main(x):
+    y = x **3
+    y += 8
+    
+    if y > 27:
+        print("YAY!")
+    
+#run program
+if __name__=="__main__":
+    main(9)
+    
